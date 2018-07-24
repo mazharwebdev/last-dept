e7701f3eb39b1ac0b47865420d5c1076c72b3ff6
junaidq <xcoder27@gmail.com>
2018-07-24 13:36:31 +0500

    Chapter#9 -Cart Creation

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
+  
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+      session[:counter] += 1     
+  end  
+
   def index
-    @products = Product.order(:title)
+    @count = increment_counter
+    @products  = Product.all
+    @shown_message = "You've been here #{@count} times"
   end
 end
