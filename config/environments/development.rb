5a7e151cd8bc41bf69031f1719020ccc4e4f5c83
junaidq <xcoder27@gmail.com>
2018-07-29 02:00:21 +0500

    chapter#13 Sending Mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,15 @@ Depot::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'testfbcon@gmail.com',
+    password:             'testfbcon1234',
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
+  
 end
